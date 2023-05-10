275612c64ea199ae0da1301545ad35e103094ded
tulliogio <tullio.giovanni84@gmail.com>
2023-05-10 15:40:12 +0100

    dia 4

--- src/App.jsx
+++ src/App.jsx
@@ -1,72 +1,121 @@
 
+
 import Header from "./components/iconos/Header";
 import TodoComputed from "./components/iconos/TodoComputed";
 import TodoFilter from "./components/iconos/TodoFilter";
 import TodoList from "./components/iconos/TodoList";
 import TodoCreate from "./components/iconos/TodoCreate";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
-const initialStateTodos = [{ id: 1, title: "Read the book", completed: true},
-  { id: 2, title: "Go to the gym", completed: false },
-  { id: 3, title: "Pick up the dog", completed: false },
-  {id:4,title:"Study",completed: false}]
+
+const initialStateTodos = JSON.parse(localStorage.getItem("todos")) || [];
+
+//https://github.com/ymulenll/react-beautiful-dnd-demo/blob/master/src/App.js
+const reorder = (list, startIndex, endIndex) => {
+    const result = [...list];
+    const [removed] = result.splice(startIndex, 1);
+    result.splice(endIndex, 0, removed);
+
+    return result;
+};
 
 const App = () => {
-  const [todos, setTodos] = useState(initialStateTodos);
-  const createTodo = (title) => {
-    const newTodo = {
-      id: Date.now(),
-      title,
-      completed: false
-    }
-    setTodos([...todos, newTodo])
-  };
+    const [todos, setTodos] = useState(initialStateTodos);
 
-  const updateTodo = (id) => {
-    setTodos(todos.map(todo => todo.id === id ? { ...todo, completed: !todo.completed } : todo))
-  };
+    useEffect(() => {
+        localStorage.setItem("todos", JSON.stringify(todos));
+    }, [todos]);
 
-  const removeTodo = (id) => {
-    setTodos(todos.filter((todo) => todo.id !== id));
-  };
+    const handleDragEnd = (result) => {
+        const { destination, source } = result;
+        if (!destination) return;
+        if (
+            source.index === destination.index &&
+            source.droppableId === destination.droppableId
+        )
+            return;
 
-  const computedItemsLeft = todos.filter((todo) => !todo.completed).length;
+        setTodos((prevTasks) =>
+            reorder(prevTasks, source.index, destination.index)
+        );
+    };
 
-  const clearCompleted = () => {
-    setTodos(todos.filter((todo) => !todo.completed));
-  }
+    const createTodo = (title) => {
+        const newTodo = {
+            id: `${Date.now()}`,
+            title: title.trim(),
+            completed: false,
+        };
 
-  const  [filter, setFilter]  = useState('all');
-  const changeFilter = (filter) => setFilter(filter);
-  const filteredTodos = () => {
-    switch (filter) {
-      case 'all':
-        return todos;
-      case 'active':
-        return todos.filter((todo) => !todo.completed);
-      case 'completed':
-        return todos.filter((todo) => todo.completed);
-      default:
-        return todos;
-    }
-  }
+        setTodos([...todos, newTodo]);
+    };
 
-  return (<div className="  mt-8 max-h-[200px] overflow-auto bg-[url('./assets/images/bg-mobile-light.jpg')] bg-no-repeat bg-contain bg-gray-300 min-h-screen dark:bg-gray-900 dark:bg-[url('./assets/images/bg-mobile-dark.jpg')]">
-  <Header />
-   <main className="container mx-auto px-4 mt-8 ">
-    <TodoCreate createTodo = {createTodo} />
-     <TodoList todos={filteredTodos()} removeTodo={removeTodo} updateTodo={updateTodo} />
-     <TodoComputed computedItemsLeft={computedItemsLeft} clearCompleted={clearCompleted} />
-     <TodoFilter changeFilter={changeFilter} filter={filter} />
-     
+    const removeTodo = (id) => {
+        setTodos(todos.filter((todo) => todo.id !== id));
+    };
 
-   </main>
- <footer className="text-center mt-8 dark:text-gray-400">drag and drop to reader list</footer>
-</div>)
-    
-  }
+    const updateTodo = (id) => {
+        setTodos(
+            todos.map((todo) =>
+                todo.id === id ? { ...todo, completed: !todo.completed } : todo
+            )
+        );
+    };
 
+    const computedItemsLeft = todos.filter((todo) => !todo.completed).length;
 
+    const clearCompleted = () => {
+        setTodos(todos.filter((todo) => !todo.completed));
+    };
 
+    const [filter, setFilter] = useState("all");
+
+    const changeFilter = (filter) => setFilter(filter);
+
+    const filteredTodos = () => {
+        switch (filter) {
+            case "all":
+                return todos;
+            case "active":
+                return todos.filter((todo) => !todo.completed);
+            case "completed":
+                return todos.filter((todo) => todo.completed);
+            default:
+                return todos;
+        }
+    };
+
+    return (
+        <div className="min-h-screen bg-gray-300 bg-mobile-light bg-contain bg-no-repeat transition-all duration-1000 dark:bg-gray-900 dark:bg-mobile-dark md:bg-desktop-light md:dark:bg-desktop-dark">
+            <Header />
+
+            <main className="container mx-auto mt-8 px-4 md:max-w-xl">
+                <TodoCreate createTodo={createTodo} />
+
+                {todos.length > 0 ? (
+                    <TodoList
+                        todos={filteredTodos()}
+                        removeTodo={removeTodo}
+                        updateTodo={updateTodo}
+                        handleDragEnd={handleDragEnd}
+                    />
+                ) : (
+                    <p>Cargando...</p>
+                )}
+
+                <TodoComputed
+                    computedItemsLeft={computedItemsLeft}
+                    clearCompleted={clearCompleted}
+                />
+
+                <TodoFilter changeFilter={changeFilter} filter={filter} />
+            </main>
+
+            <footer className="mt-8 text-center dark:text-gray-400">
+                Drag and drop to reorder list
+            </footer>
+        </div>
+    );
+};
 
 export default App;
--- src/components/iconos/TodoList.jsx
+++ src/components/iconos/TodoList.jsx
@@ -1,11 +1,40 @@
-
+import { DragDropContext, Droppable, Draggable } from "@hello-pangea/dnd";
 import TodoItem from "./TodoItem";
 
-const TodoList = ({ todos,removeTodo,updateTodo }) => {
-  return (<div className="bg-white rounded-t-md mt-8 overflow-hidden">
-    {todos.map((todo) => <TodoItem key={todo.id} todo={todo} removeTodo={removeTodo} updateTodo={updateTodo} />)}
-   
-    
-  </div>);
+const TodoList = ({ todos, removeTodo, updateTodo, handleDragEnd }) => {
+    return (
+        <DragDropContext onDragEnd={handleDragEnd}>
+            <Droppable droppableId="todos">
+                {(droppableProvided) => (
+                    <div
+                        className="mt-8 overflow-hidden rounded-t-md bg-white transition-all duration-1000 dark:bg-gray-800 [&>article]:p-4"
+                        {...droppableProvided.droppableProps}
+                        ref={droppableProvided.innerRef}
+                    >
+                        {todos.map((todo, index) => (
+                            <Draggable
+                                key={todo.id}
+                                draggableId={`${todo.id}`}
+                                index={index}
+                            >
+                                {(provided) => (
+                                    <TodoItem
+                                        {...provided.draggableProps}
+                                        ref={provided.innerRef}
+                                        {...provided.dragHandleProps}
+                                        todo={todo}
+                                        removeTodo={removeTodo}
+                                        updateTodo={updateTodo}
+                                    />
+                                )}
+                            </Draggable>
+                        ))}
+                        {droppableProvided.placeholder}
+                    </div>
+                )}
+            </Droppable>
+        </DragDropContext>
+    );
 };
+
 export default TodoList;
